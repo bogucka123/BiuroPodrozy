64a20d8679200ad8d1e7b9b32133aca8be551aa1
FIXIU <fixiu@tuta.io>
2025-04-04 14:18:19 +0200

    base program workings

--- src/Client.java
+++ src/Client.java
@@ -4,12 +4,32 @@ public class Client {
     private String email;
     private String pesel;
     private Adress adress;
-    public Client(String name, String surname, String email, String pesel, Adress adress) {
+    private Date birthDate;
+    private Client guardian;
+
+    public Client(String name, String surname, String email, String pesel,
+                  Adress adress, Date birthDate) {
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.pesel = pesel;
         this.adress = adress;
+        this.birthDate = birthDate;
+    }
 
+    public void setGuardian(Client guardian) {
+        this.guardian = guardian;
+    }
+
+    public String toString() {
+        String info = "Klient: " + name + " " + surname
+                + "\nData urodzenia: " + birthDate
+                + "\nEmail: " + email
+                + "\nPesel: " + pesel
+                + "\nAdres: " + adress;
+        if (guardian != null) {
+            info += "\nOpiekun: " + guardian.name + " " + guardian.surname;
+        }
+        return info;
     }
 }
